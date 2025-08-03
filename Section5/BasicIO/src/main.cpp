8a2a29f82db658b2d4e155e6b00694159c5cac49
Douglas Ribas de Mattos <douglasmattos0@gmail.com>
2025-08-03 10:54:52 -0300

    Section 5 - Exercises

---
+++ Section5/BasicIO/src/main.cpp
@@ -0,0 +1,23 @@
+// Section 5
+// Basic I/O using cin and cout
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hellow World!" << endl;
+
+    // cout << "Hello";
+    // cout << "World!" << endl;
+
+    // cout << "Hello world!" << endl;
+    // cout << "Hello " << "world!" << endl;
+    // cout << "Hello " << "world!\n";
+    // cout << "Hello\n"Out\nThere\n";
+
+    // int num1;
+    // int num2;
+
+}
---
+++ Section5/ModifiedProgram/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Program Name: ModifiedProgram
+ * Description : Create a C++ program that asks the user for their favorite number between 1 and 100
+ *               then read this number from the console.
+ * Author      : Douglas Ribas de Mattos
+ * Date        : 02.08.2025
+ */
+
+#include <iostream>
+
+int main() {
+    int favorite_number;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    return 0;
+}
