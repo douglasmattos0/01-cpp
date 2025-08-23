863a037c366994fd8b5dc126892449cff17397d4
Douglas Ribas de Mattos <douglasmattos0@gmail.com>
2025-08-23 20:47:54 -0300

    Section11 - Exercises Completed

---
+++ Section11/Random/src/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * Program Name: Random
+ * Description : Demonstrates basic mathematical functions operations in C++.
+ * Author      : Douglas Ribas de Mattos
+ * Date        : 19.08.2025
+ */
+// Section 11
+// Random Numbers
+
+#include <iostream>
+#include <cstdlib>      // required for rand()
+#include <ctime>       // required for time( )  
+
+using namespace std;
+
+int main() {
+      
+    int random_number {};
+    size_t count {10};           // number of random numbers to generate
+    int min {1};                  // lower bound (inclusive) 
+    int max {6};                 // upper bound (inclusive)
+    
+    // seed the random number generator
+    // If you don't seed the generator you will get the same requence random numbers every run
+    
+    cout << "RAND_MAX on my system is: " << RAND_MAX << endl;
+    srand(time(nullptr));  
+    
+    for (size_t i{1}; i<=count; ++i)  {
+        random_number = rand() % max + min;     // generate a random number [min, max]
+        cout << random_number << endl;
+    }
+
+    cout  << endl;
+    return 0;
+}
